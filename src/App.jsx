ddc9183aa42afa9a3184da2c287e236cb9c12d84
abdallah <abdallahradfi@gmail.com>
2023-08-24 13:03:31 +0100

    add the select button to choose between pomodoro, short break or long break

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,41 @@ import {Home} from "./pages/Home";
 import {Stats} from "./pages/Stats";
 import { useState, useEffect } from "react";
 
-function App() {
-  let initialTime = 25*60;
-  const timerStyle = {
-    pomodoro: "pomodoro",
-    shortBreak: "break",
-    longBreak: "long break"
+export const timerStyle = {
+  pomodoro: {
+    time: 25,
+    title: "Pomodoro"
+  },
+  shortBreak: {
+    time: 5,
+    title: "Short Break"
+  },
+  longBreak: {
+    time: 15,
+    title: "Long Break"
   }
+}
 
-  const [timeRemaining, setTimeRemaining] = useState(initialTime);
+export function App() {
+
+  const [timeRemaining, setTimeRemaining] = useState(timerStyle.pomodoro.time);
   const [isRunning, setIsRunning] = useState(false);
-  const [isPomodoro, setIsPomodoro] = useState(timerStyle.pomodoro);
+  const [isPomodoro, setIsPomodoro] = useState(timerStyle.pomodoro.title);
 
   useEffect(()=>{
     let timer;
 
-    if(timeRemaining>0 && isRunning){
+    if(timeRemaining >= 0 && isRunning){
       timer = setTimeout(() => {
         setTimeRemaining(timeRemaining - 1);
       }, 1000)
-    }else if(timeRemaining===0){
-      setIsPomodoro(pre => !pre);
-      setTimeRemaining(initialTime);
-      setIsRunning(false)
+    }else if(timeRemaining < 0){
+      if(isPomodoro===timerStyle.pomodoro.title){
+        setTimeRemaining(timerStyle.shortBreak.time);
+        setIsPomodoro(timerStyle.shortBreak.title);
+      }else{
+        resetTimer();
+      }
     }
     
     return () => clearTimeout(timer);
@@ -35,25 +47,44 @@ function App() {
 
   const resetTimer = () => {
     setIsRunning(false);
-    setTimeRemaining(initialTime);
+    setTimeRemaining(timerStyle.pomodoro.time);
+    setIsPomodoro(timerStyle.pomodoro.title);
+  }
+
+  const reducer = (event)=>{
+    switch(event.target.value){
+      case timerStyle.pomodoro.title:
+        setTimeRemaining(timerStyle.pomodoro.time);
+        setIsPomodoro(timerStyle.pomodoro.title); 
+        break;
+      case timerStyle.shortBreak.title:
+        setTimeRemaining(timerStyle.shortBreak.time);
+        setIsPomodoro(timerStyle.shortBreak.title);
+        break;
+      case timerStyle.longBreak.title:
+        setTimeRemaining(timerStyle.longBreak.time);
+        setIsPomodoro(timerStyle.longBreak.title);
+        break;
+    }
+    setIsRunning(true);
   }
 
   return (
     <div className="wrapper">
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">Pomoraf</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <div className="container-fluid px-5">
+          <a className="navbar-brand logo" href="/">Pomoraf</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
                 <Link className="nav-link active" to="/">Home</Link>
               </li>
-              <li class="nav-item">
-                <Link className="nav-link active" to="/stats">Stats</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/stats">Stats</Link>
               </li>
             </ul>
           </div>
@@ -61,12 +92,10 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={
-          <Home timeRemaining={timeRemaining} isRunning={isRunning} stopTimer={stopTimer} resetTimer={resetTimer}/>
+          <Home timeRemaining={timeRemaining} isRunning={isRunning} stopTimer={stopTimer} resetTimer={resetTimer} reducer={reducer}/>
         } />
         <Route path="/stats" element={<Stats />} />
       </Routes>
     </div>
   )
 }
-
-export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
-export const Home = ({ timeRemaining, isRunning, stopTimer, resetTimer}) => {
+import { timerStyle } from '../App';
+
+export const Home = ({ timeRemaining, isRunning, stopTimer, resetTimer, reducer}) => {
 
     let minutes = Math.floor(timeRemaining / 60).toString();
     let seconds = (timeRemaining % 60).toString();
@@ -6,13 +8,31 @@ export const Home = ({ timeRemaining, isRunning, stopTimer, resetTimer}) => {
     seconds = (seconds.length == 1) ? `0${seconds}` : seconds;
 
     return (
-        <div>
-            <div className="timer">
+        <div className="card text-center">
+
+            {/* <div className="dropdown">
+                <button className="btn btn-outline-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <ul className="dropdown-menu">
+                    <li onClick={()=>console.log("Action")} sel><a className="dropdown-item">Pomodoro</a></li>
+                    <li onClick={()=>console.log("Another Action")}><a className="dropdown-item">Short Break</a></li>
+                    <li onClick={()=>console.log("somtet Action")}><a className="dropdown-item">Long Break</a></li>
+                </ul>
+            </div> */}
+
+            <select name="timeStyle">
+                <option onClick={reducer} value={timerStyle.pomodoro.title}>{timerStyle.pomodoro.title}</option>
+                <option onClick={reducer} value={timerStyle.shortBreak.title}>{timerStyle.shortBreak.title}</option>
+                <option onClick={reducer} value={timerStyle.longBreak.title}>{timerStyle.longBreak.title}</option>
+            </select>
+
+            <div className="fs-1">
                 {minutes}:{seconds}
             </div>
             <div className="btns">
-                <button onClick={stopTimer}>{isRunning ? "||" : ">"}</button>
-                <button onClick={resetTimer}>Reset</button>
+                <button className="btn btn-outline-dark fs-5" onClick={stopTimer}>{isRunning ? "||" : ">"}</button>
+                <button className="btn btn-outline-dark fs-5" onClick={resetTimer}>Reset</button>
             </div>
         </div>
     )
