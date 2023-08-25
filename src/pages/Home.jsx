e0094f87e22e9420dc5a697acfbc98ebb1db1e94
abdallah <abdallahradfi@gmail.com>
2023-08-25 13:12:17 +0100

    fix some bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,22 +2,25 @@ import { timerStyle } from '../App';
 
 export const Home = ({ stopTimer, resetTimer, state, changeTimerType }) => {
 
-    let minutes = Math.floor(state.timeRemaining / 60).toString();
-    let seconds = (state.timeRemaining % 60).toString();
-    minutes = (minutes.length == 1) ? `0${minutes}` : minutes;
-    seconds = (seconds.length == 1) ? `0${seconds}` : seconds;
+    const timerFormatter = () => {
+        let minutes = Math.floor(state.timeRemaining / 60).toString();
+        let seconds = (state.timeRemaining % 60).toString();
+        minutes = (minutes.length == 1) ? `0${minutes}` : minutes;
+        seconds = (seconds.length == 1) ? `0${seconds}` : seconds;
+        return `${minutes}:${seconds}`
+    }
     
     return (
         <div className="card text-center">
 
-            <select name="timeStyle" onChange={changeTimerType}>
-                <option selected={ state.isPomodoro == timerStyle.pomodoro.title } value={timerStyle.pomodoro.title}>{timerStyle.pomodoro.title}</option>
-                <option selected={ state.isPomodoro == timerStyle.shortBreak.title } value={timerStyle.shortBreak.title}>{timerStyle.shortBreak.title}</option>
-                <option selected={ state.isPomodoro == timerStyle.longBreak.title } value={timerStyle.longBreak.title}>{timerStyle.longBreak.title}</option>
+            <select name="timeStyle" value={state.isPomodoro} onChange={changeTimerType}>
+                <option value={timerStyle.pomodoro.title}>{timerStyle.pomodoro.title}</option>
+                <option value={timerStyle.shortBreak.title}>{timerStyle.shortBreak.title}</option>
+                <option value={timerStyle.longBreak.title}>{timerStyle.longBreak.title}</option>
             </select>
 
             <div className="fs-1">
-                {minutes}:{seconds}
+                {timerFormatter()}
             </div>
             <div className="d-grid gap-2 d-md-block">
                 <button className="btn btn-outline-dark fs-5" onClick={stopTimer}>{state.isRunning ? "||" : ">"}</button>
