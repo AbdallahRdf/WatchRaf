581e4230502a0b918c29a34c4bdc54e7dc920a53
abdallah <abdallahradfi@gmail.com>
2023-08-30 18:03:31 +0100

    sign up with email and password finished

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,44 +1,100 @@
-import { signInWithPopup } from "firebase/auth"
+import { signInWithPopup, createUserWithEmailAndPassword } from "firebase/auth"
 import { auth, googleAuth } from "../firebase/firebase";
 import { useNavigate } from "react-router-dom";
-import googleImg from '../img/google-logo.png'
+import googleImg from '../img/google-logo.png';
+
+import * as yup from "yup";
+import {yupResolver} from '@hookform/resolvers/yup';
+import { useForm } from "react-hook-form";
 
 export const Signup = () => {
+
     const navigate = useNavigate();
 
     const loginWithGoogle = async () => {
-        const result = await signInWithPopup(auth, googleAuth);
-        navigate('/');
+        try {
+            const result = await signInWithPopup(auth, googleAuth);
+            navigate('/');
+        } catch (error) {
+            console.error("Signup error: ", error.message);
+        }
     }
 
-  return (
-    <div className="wrapper">
-        <div>
-            <a href="/" className="logo-large">Pomoraf</a>
-            <p className="grey-signup-text">Create Account</p>
+    //* creating form schema for inputs validation using yup!
+    const schema = yup.object().shape({
+        email: yup.string().email("Invalid email").required("Email is required"),
+        password: yup
+            .string()
+            .min(6, "Password must be at least 6 characters")
+            .required("Password is required"),
+        confirmPassword: yup
+            .string()
+            .oneOf([yup.ref('password'), null], 'Password must match')
+    });
+
+    const { register, handleSubmit, formState: { errors }, getValues } = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    const createUser = async () => {
+        const {email, password} = getValues();
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            console.log("User signed up: ", user);
+        } catch(error) {
+            console.error("Signup error: ", error.message);
+        }
+    }
+
+
+    return (
+        <div className="wrapper">
+            <div>
+                <a href="/" className="logo-large">Pomoraf</a>
+                <p className="grey-signup-text">Create Account</p>
+            </div>
+            <form onSubmit={handleSubmit(createUser)} className="form-wrapper">
+                <div className="w-100 mb-3 mt-3">
+                    <button className="google-signup-btn" onClick={loginWithGoogle}>
+                        <img className="google-logo" src={googleImg} alt="google logo" />
+                        Login with Google
+                    </button>
+                </div>
+                <p className="or-line" >or</p>
+                <div className="w-100 mt-3">
+                    <label className="form-label text-body-tertiary" htmlFor="email">Email</label>
+                    <input
+                        className="form-control mb-3"
+                        type="email"
+                        id="Email"
+                        placeholder="example@mail.com"
+                        {...register('email')}
+                    />
+                    <small className="text-danger">{errors.email?.message}</small>
+                </div>
+                <div className="w-100">
+                    <label className="form-label text-body-tertiary" htmlFor="password">Password</label>
+                    <input
+                        className="form-control mb-3"
+                        type="password"
+                        id="password"
+                        {...register('password')}
+                    />
+                    <small className="text-danger">{errors.password?.message}</small>
+                </div>
+                <div className="w-100">
+                    <label className="form-label text-body-tertiary" htmlFor="confirmPassword">Confirm your password</label>
+                    <input
+                        className="form-control mb-3"
+                        type="password"
+                        id="confirmPassword"
+                        {...register('confirmPassword')}
+                    />
+                    <small className="text-danger">{errors.confirmPassword?.message}</small>
+                </div>
+                <input className="signup-btn" type="submit" value="Sign up" />
+            </form>
         </div>
-        <form className="form-wrapper">
-            <div className="w-100 mb-3 mt-3"> 
-                <button className="google-signup-btn" onClick={loginWithGoogle}>
-                      <img className="google-logo" src={googleImg} alt="google logo" />
-                    Login with Google
-                </button>
-            </div>
-            <p className="or-line" >or</p>
-            <div className="w-100 mt-3"> 
-                <label className="form-label text-body-tertiary" htmlFor="email">Email</label>
-                <input className="form-control mb-3" type="email" name="email" id="Email" placeholder="example@mail.com" />
-            </div>
-            <div className="w-100"> 
-                <label className="form-label text-body-tertiary" htmlFor="password">Password</label>
-                <input className="form-control mb-3" type="password" name="password" id="password" />
-            </div>
-            <div className="w-100"> 
-                <label className="form-label text-body-tertiary" htmlFor="confirmPassword">Confirm your password</label>
-                <input className="form-control mb-3" type="password" name="confirmPassword" id="confirmPassword" />
-            </div>
-            <input className="signup-btn" type="submit" value="Sign up" />
-        </form>
-    </div>
-  )
+    )
 }
