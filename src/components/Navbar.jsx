afbe7e272122421ee75f168c8ba9228b3d21c497
abdallah <abdallahradfi@gmail.com>
2023-09-11 19:29:20 +0100

    started some responsive stuff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import { Link, useLocation } from "react-router-dom";
 import { auth } from "../firebase/firebase";
 import { signOut } from "firebase/auth";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import MyContext from "../context/MyContext";
+import menuIcon from '../img/hamburger.png';
 
 const Navbar = ({user}) => {
 
-    const { setBreakData , setPomoData } = useContext(MyContext);
+    const { setBreakData, setPomoData, screenWidth } = useContext(MyContext);
+
+    const isScreenSmall = screenWidth <= 768;
     
     const signUserOut = () => {
         setPomoData([]);
@@ -21,28 +24,58 @@ const Navbar = ({user}) => {
     }
 
     return (
-        <nav className="nav">
-            <div>
-                <a href="/" className="logo">Pomoraf</a>
-            </div>
-            <ul className="list">
-                <li className="list-item">
-                    <Link to="/" className={location.pathname === "/" ? "list-link active-link" : "list-link"}>Home</Link>
-                </li>
-                <li className="list-item">
-                    <Link to="/stats" className={location.pathname === "/stats" ? "list-link active-link" : "list-link"}>Stats</Link>
-                </li>
-                {user ?
-                    <li className="list-item">
-                        <button onClick={signUserOut} className={"list-link"}>Log-out</button>
-                    </li>
+        <>
+            <nav className="nav">
+                <div>
+                    <a href="/" className="logo">Pomoraf</a>
+                </div>
+                {
+                    !isScreenSmall
+                    ?
+                        <ul className="list">
+                            <li className="list-item">
+                                <Link to="/" className={location.pathname === "/" ? "list-link active-link" : "list-link"}>Home</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link to="/stats" className={location.pathname === "/stats" ? "list-link active-link" : "list-link"}>Stats</Link>
+                            </li>
+                            {user ?
+                                <li className="list-item">
+                                    <button onClick={signUserOut} className={"list-link"}>Log-out</button>
+                                </li>
+                                :
+                                <li className="list-item">
+                                    <Link to="/signup" className={location.pathname === "/signup" ? "list-link active-link" : "list-link"}>Sign-up</Link>
+                                </li>
+                            }
+                        </ul>
                     :
-                    <li className="list-item">
-                        <Link to="/signup" className={location.pathname === "/signup" ? "list-link active-link" : "list-link"}>Sign-up</Link>
-                    </li>
+                        <button className="menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                            <img src={menuIcon} alt="menu icon" className="menu-img"/>
+                        </button>
                 }
-            </ul>
-        </nav>
+            </nav>
+            {   
+                isScreenSmall &&
+                <ul className="collapse w-75 m-auto" id="collapseExample">
+                    <li className="list-item">
+                        <Link to="/" className={location.pathname === "/" ? "list-link active-link" : "list-link"}>Home</Link>
+                    </li>
+                    <li className="list-item">
+                        <Link to="/stats" className={location.pathname === "/stats" ? "list-link active-link" : "list-link"}>Stats</Link>
+                    </li>
+                    {user ?
+                        <li className="list-item">
+                            <button onClick={signUserOut} className={"list-link"}>Log-out</button>
+                        </li>
+                        :
+                        <li className="list-item">
+                            <Link to="/signup" className={location.pathname === "/signup" ? "list-link active-link" : "list-link"}>Sign-up</Link>
+                        </li>
+                    }
+                </ul>
+            }
+        </>
     )
 }
 
---
+++ src/hooks/useScreenWidth.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export const useScreenWidth = () => {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleScreenSizeChange = () => {
+        setScreenWidth(window.innerWidth);
+      };
+      window.addEventListener("resize", handleScreenSizeChange);
+
+      return () => window.removeEventListener("resize", handleScreenSizeChange);
+    }, []);
+
+    return screenWidth;
+}
